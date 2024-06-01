a0fd7963ec2428d56875466634aeb4d787239240
Rimesh Jotaniya <rimesh.j@icloud.com>
2024-06-01 22:20:51 +0200

    version 0.0.1

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkSDK",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,10 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NetworkSDK"),
-        .testTarget(
-            name: "NetworkSDKTests",
-            dependencies: ["NetworkSDK"]),
+        .binaryTarget(
+            name: "NetworkSDK",
+            path: "./Sources/NetworkSDK.xcframework"
+        )
     ]
 )
---
+++ Sources/NetworkSDK.xcframework/ios-arm64_x86_64-simulator/NetworkSDK.framework/Headers/NetworkSDK.h
@@ -0,0 +1,18 @@
+//
+//  NetworkSDK.h
+//  NetworkSDK
+//
+//  Created by Rimesh Jotaniya on 01/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkSDK.
+FOUNDATION_EXPORT double NetworkSDKVersionNumber;
+
+//! Project version string for NetworkSDK.
+FOUNDATION_EXPORT const unsigned char NetworkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkSDK/PublicHeader.h>
+
+
